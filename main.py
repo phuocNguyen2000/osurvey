7fd622d54ae0b9f62e3e9d4750fa58bcd080ec0b
phuocNguyen2000 <phuocnguyen822@gmail.com>
2021-10-31 12:56:38 +0700

    ok

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 from datetime import datetime
 import secrets
 import json
-from waitress import serve
+
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=8080,threads =  6)
+    app.run(debug=True)

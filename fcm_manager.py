eda0197f52564a0499c0212397d50e7b6cde10d1
phuocNguyen2000 <phuocnguyen822@gmail.com>
2021-11-01 20:22:30 +0700

    ok

--- fcm_manager.py
+++ fcm_manager.py
@@ -11,7 +11,7 @@ key={
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-styad%40esurvey-ee4c3.iam.gserviceaccount.com"
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-styad"+"%40"+"esurvey-ee4c3.iam.gserviceaccount.com"
 }
  
 from firebase_admin import credentials 

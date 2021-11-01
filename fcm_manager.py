ca0309062d971498ec20b2d854ae93398bd1e891
phuocNguyen2000 <phuocnguyen822@gmail.com>
2021-11-01 22:23:26 +0700

    ok

--- fcm_manager.py
+++ fcm_manager.py
@@ -4,7 +4,35 @@ import re
 
 
 from firebase_admin import credentials
-s="-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDNUmkJpLA0JzHY\nKtIFfHbkrc2ljCzrJYGaYzcNbRkE0JvZiGwXVJE2Pi7MJKL7twOZ0t7/CmWaEuiv\n5cVkG3l6oAJJBf/duZrdjmZ91vBJsfZmFDQbuvt46Pn+naZzg5MGz8YOHSOaNWoK\nQb7mDVWPGvKgxleunVFoQ5lwovhUaSBUJhwxEWKHYNnX2BDPqz2NiyXTtHD3sluT\nPM06dkPfokZrCB3hP9LlervctP/UvtMZcQfLWCpn9eOaACaGXWf/TJ81H3Vu7Kau\npl06nQtLiZ/fVAn1TO5CAKcY7dRimwwSQGRBphCUUNTeyfTNLH9iRzQTkeaFyIpR\nxP/pOoqpAgMBAAECggEAVacA2XyB4yfMo9RtK8u533jf1qVM4cWSNOEZyLiP3NHX\nih5elTb6oum65P+NnDtDPcrZqwRf0S/9MM/GEhZiDXUtrt/ZVtpEUBPFDs75C405\ncGkSOdU9bbmm/IAwvXSQg6Fuilm+jeildWTq6ZZ7cEj6GXbUgivvl3LH0lSyCqfX\nAZbVVDA5pyILONUU+XtGu7sY2r7KdbdSNj/4ltxiA7E0XWqAf+sEfbgjO/572O5l\nqmG/Ch/b/DzmbX0ODHZ3y1cbD3ZObEIb5Y9nrIaybzLSYgj1c7YqAn9eeQ4Hq5Au\nvrn5ubFxgOMfhclZrlOxHth4tX+G5U98ZbmUVe0zLwKBgQDnUhagnbdC5m+5eAXE\niwxCp2rzoF9JzIwarwHCSQL5HqnIyOfc8C/ahlwwhsoYTXrq5BYxrbMVsCkZjYKE\nGdWAeoMYDx7u+gEojJhUNfKJudTeQmX+aW8pGgMEubTaWfYwDKuEU5qq2WjXMqQs\nrXC/TAMMm1lVuue/ztBP2shy+wKBgQDjOjwtNwU28hrfEp6IRAArRGuT/gJq8XiI\niZtMgBMOrOIl+TgqLw8+0BF33zFTbqeyD1pYdKKMCNbrVVAPkBgeHHF8fLiNSXZk\nmxs//RxXbIeVuMimsBpWYbNcgU+tTnAhHO7B+nWSLTtL2LOSjpi6/iT1V1Ls7uf8\nMsFuFrynqwKBgBwpr12Qth4sBhAzn6glMbHvxiKxNMi8YZZiFjm8P+Nqq0spwqjw\niTL2xsSVtIcu77cnW9hiiHosf2SACRLiPk8tG0bTmHWJ9JgmPemKw+Omv1bsCJTn\n60O6ygFKRs2KxGFnOX6b2ynP3GbO4JQb7a0sqPZg94hrgAmnOU9vmJX/AoGBAKf3\ne1Y9DrjAHMb7H39BLRLOv+sk4Cqnlt7vQYI6RlwZxg4l/LOKF+3pppACx5aR/Jpu\nKO2sQh/bxOsvJEBs1rcjdWx9UXr3a/IQigMyGgox6sPtVT2Kfd2O2SQvXnOQwOhp\nV8DCYLiOgP+fD5btm87WYQGO7HjCXXAbIMs9OXWDAoGACGCU0JCEyjKf6ErFPnNx\npwXrsqVAILwaSCXUu5EBU+aUgoM/e0WKwudiur4EXxEXi4agys/fZJBHXNxfCvGy\n4vIg7QQ2mY/48VSZvRRi78NEsEgfWiHak3aOsgxyydlYKwJWlJM3GXS64AcM34Av\nwkpCt+DTKgCS0aPUbVqAoj8=\n-----END PRIVATE KEY-----\n"
+s="""-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDNUmkJpLA0JzHY
+KtIFfHbkrc2ljCzrJYGaYzcNbRkE0JvZiGwXVJE2Pi7MJKL7twOZ0t7/CmWaEuiv
+5cVkG3l6oAJJBf/duZrdjmZ91vBJsfZmFDQbuvt46Pn+naZzg5MGz8YOHSOaNWoK
+Qb7mDVWPGvKgxleunVFoQ5lwovhUaSBUJhwxEWKHYNnX2BDPqz2NiyXTtHD3sluT
+PM06dkPfokZrCB3hP9LlervctP/UvtMZcQfLWCpn9eOaACaGXWf/TJ81H3Vu7Kau
+pl06nQtLiZ/fVAn1TO5CAKcY7dRimwwSQGRBphCUUNTeyfTNLH9iRzQTkeaFyIpR
+xP/pOoqpAgMBAAECggEAVacA2XyB4yfMo9RtK8u533jf1qVM4cWSNOEZyLiP3NHX
+ih5elTb6oum65P+NnDtDPcrZqwRf0S/9MM/GEhZiDXUtrt/ZVtpEUBPFDs75C405
+cGkSOdU9bbmm/IAwvXSQg6Fuilm+jeildWTq6ZZ7cEj6GXbUgivvl3LH0lSyCqfX
+AZbVVDA5pyILONUU+XtGu7sY2r7KdbdSNj/4ltxiA7E0XWqAf+sEfbgjO/572O5l
+qmG/Ch/b/DzmbX0ODHZ3y1cbD3ZObEIb5Y9nrIaybzLSYgj1c7YqAn9eeQ4Hq5Au
+vrn5ubFxgOMfhclZrlOxHth4tX+G5U98ZbmUVe0zLwKBgQDnUhagnbdC5m+5eAXE
+iwxCp2rzoF9JzIwarwHCSQL5HqnIyOfc8C/ahlwwhsoYTXrq5BYxrbMVsCkZjYKE
+GdWAeoMYDx7u+gEojJhUNfKJudTeQmX+aW8pGgMEubTaWfYwDKuEU5qq2WjXMqQs
+rXC/TAMMm1lVuue/ztBP2shy+wKBgQDjOjwtNwU28hrfEp6IRAArRGuT/gJq8XiI
+iZtMgBMOrOIl+TgqLw8+0BF33zFTbqeyD1pYdKKMCNbrVVAPkBgeHHF8fLiNSXZk
+mxs//RxXbIeVuMimsBpWYbNcgU+tTnAhHO7B+nWSLTtL2LOSjpi6/iT1V1Ls7uf8
+MsFuFrynqwKBgBwpr12Qth4sBhAzn6glMbHvxiKxNMi8YZZiFjm8P+Nqq0spwqjw
+iTL2xsSVtIcu77cnW9hiiHosf2SACRLiPk8tG0bTmHWJ9JgmPemKw+Omv1bsCJTn
+60O6ygFKRs2KxGFnOX6b2ynP3GbO4JQb7a0sqPZg94hrgAmnOU9vmJX/AoGBAKf3
+e1Y9DrjAHMb7H39BLRLOv+sk4Cqnlt7vQYI6RlwZxg4l/LOKF+3pppACx5aR/Jpu
+KO2sQh/bxOsvJEBs1rcjdWx9UXr3a/IQigMyGgox6sPtVT2Kfd2O2SQvXnOQwOhp
+V8DCYLiOgP+fD5btm87WYQGO7HjCXXAbIMs9OXWDAoGACGCU0JCEyjKf6ErFPnNx
+pwXrsqVAILwaSCXUu5EBU+aUgoM/e0WKwudiur4EXxEXi4agys/fZJBHXNxfCvGy
+4vIg7QQ2mY/48VSZvRRi78NEsEgfWiHak3aOsgxyydlYKwJWlJM3GXS64AcM34Av
+wkpCt+DTKgCS0aPUbVqAoj8=
+-----END PRIVATE KEY-----
+"""
 g=re.sub('/\\n/g', '\n', s)
 print(s)
 print(g)
@@ -36,5 +64,10 @@ def sendPush(title,msg,re_token,dataObj=None):
         tokens=re_token
     )
 
-    response=messaging.send_multicast(message)
+    try:
+        response=messaging.send_multicast(message)
+    finally:
+        print("ok")
+    
+    
     print(response)
